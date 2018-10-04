7eb38ce574dd05594b6e24b69e1876605f768b0a
Akash Dixit <anshudixit00011@gmail.com>
2018-10-04 22:27:47 +0530

    p1

---
+++ Pattern1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class P{
+    public static void printStars(int n){
+        int i,j,k=2*n-2;
+        
+        for(i=0;i<n;i++){
+            for(j=0;j<k;j++){
+                System.out.println(" ");
+            }
+            k = k-2;
+            for(j=0;j<i;j++){
+                System.out.println("*");
+            }
+             System.out.println(); 
+        }
+    }
+    public static void main(String args[]){
+        int n=5;
+        printStars(n);
+    }
+}
